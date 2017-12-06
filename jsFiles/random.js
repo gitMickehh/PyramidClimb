861f1cfdc369950dfe5830d19d130181e1e0f433
N man <nader.ma@live.com>
2017-12-06 14:34:22 +0200

    fixed random generation problem \n things were appearing inside the walls

--- jsFiles/random.js
+++ jsFiles/random.js
@@ -1,47 +1,57 @@
-let x = 0;
-let y = 0;
 
-let lastX = 0;
-let row = 200;
+var x =0 ;
+var y =0 ;
 
-function generateLevel() {
+var lastX=0;
 
+var row =200;
 
+var max  ;
+var min ;
 
-    game.time.events.repeat(Phaser.Timer.SECOND * 3, 40, createFuel, this);
-    game.time.events.repeat(Phaser.Timer.SECOND * 4, 40, createSheild, this);
-    game.time.events.repeat(Phaser.Timer.SECOND * 1, 40, createEnemy, this);
-
-
-}
-
-function generateX() {
-    //max 580
-    //min 80
+function generateLevel () {
     
-    x = 0;
-    x = Math.floor(Math.random() * 580) + 80;
+    
+   max =(game.world.width) - (wallWidthToScale) - (214*gameScale * 2) ;
+   min = wallWidthToScale + ((214 * gameScale) / 2);
+    
+     game.time.events.repeat(Phaser.Timer.SECOND * 3, 40, createFuel, this);
+     game.time.events.repeat(Phaser.Timer.SECOND * 4, 40, createSheild, this);
+     game.time.events.repeat(Phaser.Timer.SECOND * 1, 40, createEnemy ,this);
+    
 
-    if (Math.abs(x - lastX) < 200 && lastX + 200 > 580) {
-        x -= 200;
-        if (x < 80) {
-            x += (80 - x);
-        }
-    } else if (Math.abs(x - lastX) < 200 && lastX - 200 < 100) {
-        x += 200;
-        if (x > 580) {
-            x -= (x - 580);
-        }
-    }
-    lastX = x;
+       }
+
+function generateX(){
+    //console.log(game.world.x);
+   // x=0;
+    x =Math.floor(Math.random() * max) + min;
+   
+    
+    if(Math.abs(x-lastX)<200 && lastX+200>max){
+       x-=200;
+       if (x<min){
+           x+=(min-x);
+           }
+       }
+    
+     else if(Math.abs(x-lastX)<200 && lastX-200<100){
+       x+=200;
+       if (x>max){
+           x-=(x-max);
+           }
+       }
+    lastX=x;
     return x;
 }
 
-function generateY() {
-
-    row += 100;
-    y = row;
-
-
+function generateY (){
+    
+    row +=100;
+    y=row;
+    
+   
     return y;
 }
+
+
